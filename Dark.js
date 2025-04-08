3cbe892c900edd390c51ab85234677239fe573f2
krishna kant behera <krishnakantbehera137@gmail.com>
2025-04-08 17:27:41 +0530

    localstorage add logic

--- Dark.js
+++ Dark.js
@@ -1,5 +1,13 @@
 let btn = document.querySelector(".btn");
 
+if (localStorage.getItem("thems") === "dark") {
+  document.body.classList.add("dark");
+}
 btn.addEventListener("click", () => {
   document.body.classList.toggle("dark");
+  if (document.body.classList.contains("dark")) {
+    localStorage.setItem("thems", "dark");
+  } else {
+    localStorage.setItem("thems", "light");
+  }
 });
